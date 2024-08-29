32e3702b6bf0c2094c9ebad71f3005d098bbb724
nitchmaa <72037550+nitchmaa@users.noreply.github.com>
2024-08-29 16:07:38 -0400

    Update lab1step18.ino

--- lab1step18/lab1step18.ino
+++ lab1step18/lab1step18.ino
@@ -1,9 +1,11 @@
 const int ledPins[] = {2, 3, 4, 5,6,7,8,9}; // LED pins
+int inputNumber;
 
 void setup() {
   // set up serial monitor
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(1000);
+  Serial.println("Enter a number between 0 and 255:");
   // Set all LED pins as output
   for (int i = 0; i < 8; i++) {
     pinMode(ledPins[i], OUTPUT);
@@ -12,8 +14,21 @@ void setup() {
 
 void loop() {
     for (int count = 0; count < 256; count++) {
+
+   if (Serial.available() > 0) {
+    int incomingByte = Serial.parseInt(); // Read the incoming number as an integer
+    if (incomingByte >= 0 && incomingByte <= 255) {
+      inputNumber = incomingByte;
+      Serial.print("You entered: ");
+      Serial.println(inputNumber);
+    } else {
+      Serial.println("Invalid input. Please enter a number between 0 and 255.");
+    }
+  }
+
     displayBinary(count);
-    delay(250); // Wait for 1 second
+    if(count==inputNumber)delay(1000);
+    //delay(0); // Wait for 1 second
   }
 }
 
