854d8637e9c0d98dedb188d4b1af3a4b7edd848a
Paigerabideaux <rabideap@mail.gvsu.edu>
2024-08-29 11:40:17 -0400

    update
    update

--- lab1step16/lab1step16.ino
+++ lab1step16/lab1step16.ino
@@ -1,6 +1,8 @@
 const int ledPins[] = {0,1, 2, 3, 4, 5,6,7}; // LED pins
 
 void setup() {
+ // Initialize serial communication at 9600 bits per second
+    Serial.begin(9600);
   // Set all LED pins as output
   for (int i = 0; i < 8; i++) {
     pinMode(ledPins[i], OUTPUT);
@@ -8,7 +10,7 @@ void setup() {
 }
 
 void loop() {
-  for (int count = 0; count < 16; count++) {
+  for (int count = 0; count < 256; count++) {
     displayBinary(count);
     delay(1000); // Wait for 1 second
   }
